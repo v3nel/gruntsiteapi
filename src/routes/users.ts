1655b273311a28c69c41f6cbc0bdae48038614ff
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-19 21:35:18 +0100

    Add basic user routes for login and CRUD operations

---
+++ src/routes/users.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response } from "express";
+
+const userRouter = Router();
+
+userRouter.post("/login", (req: Request, res: Response) => {
+    
+})
+
+userRouter.get("/", (req: Request, res: Response) => {
+
+})
+
+userRouter.post("/",  (req: Request, res: Response) => {
+    
+})
