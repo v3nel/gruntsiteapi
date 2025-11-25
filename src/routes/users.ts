009bf2abf72c58ee05e3be85fe36ad4f089bffbb
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-25 15:24:28 +0100

    Refactor user routes to simplify endpoints and integrate database queries

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,20 +1,24 @@
 import { Router, Request, Response } from "express";
 
+import { db } from "../db/index.js";
+import { users } from "../db/schemas.js";
+
 const userRouter = Router();
 
 userRouter.post("/login", (req: Request, res: Response) => {
     
 });
 
-userRouter.get("/users", (req: Request, res: Response) => {
-
+userRouter.get("/", async (res: Response) => {
+    const userTable = await db.select().from(users)
+    return res.status(200).json(userTable)
 });
 
-userRouter.post("/users",  (req: Request, res: Response) => {
+userRouter.post("/",  (req: Request, res: Response) => {
     
 });
 
-userRouter.patch("/users", (req: Request, res: Response) => {
+userRouter.patch("/", (req: Request, res: Response) => {
 
 });
 
