1c9122682744c29502221f33a0c03a29e292c39b
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-25 15:34:40 +0100

    GET - /users (#4)
    * Refactor user routes to simplify endpoints and integrate database queries
    
    * Update src/routes/users.ts

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,20 +1,28 @@
 import { Router, Request, Response } from "express";
 
+import { db } from "../db/index.js";
+import { users } from "../db/schemas.js";
+
 const userRouter = Router();
 
 userRouter.post("/login", (req: Request, res: Response) => {
     
 });
 
-userRouter.get("/users", (req: Request, res: Response) => {
-
+userRouter.get("/", async (req: Request, res: Response, next) => {
+    try {
+        const userTable = await db.select().from(users);
+        return res.status(200).json(userTable);
+    } catch (error) {
+        next(error);
+    }
 });
 
-userRouter.post("/users",  (req: Request, res: Response) => {
+userRouter.post("/",  (req: Request, res: Response) => {
     
 });
 
-userRouter.patch("/users", (req: Request, res: Response) => {
+userRouter.patch("/", (req: Request, res: Response) => {
 
 });
 
