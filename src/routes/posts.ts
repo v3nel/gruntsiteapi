94fec96e3e696483e745dd4b1fc3fb1e5477a0d7
venel._ <108694538+v3nel@users.noreply.github.com>
2025-11-28 18:18:43 +0100

    Added the GET - /posts/:id endpoint

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -2,14 +2,26 @@ import { NextFunction, Router, Request, Response } from "express";
 
 import { db } from "../db/index.js";
 import { posts } from "../db/schemas.js";
+import { eq } from "drizzle-orm";
 
 const postRouter = Router();
 
-postRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+postRouter.get("/", async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const q = db.select().from(posts) // Arranger le schema afin de pouvoir les ordonner par date
+        const q = await db.select().from(posts) // Arranger le schema afin de pouvoir les ordonner par date
         return res.status(200).json({data: q})
     } catch(err) {
         next(err);
+    }
+})
+
+postRouter.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const post = await db.select().from(posts).where(eq(posts.id, id));
+
+        return res.status(200).json({data: post});
+    } catch(err) {
+        next(err)
     }
 })
