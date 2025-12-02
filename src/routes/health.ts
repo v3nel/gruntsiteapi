a91fad8f37bf2a425160c990af5800b3073018d6
Loan Nguyen <venel@vmi2942916.contaboserver.net>
2025-12-02 20:02:07 +0100

    docs: Documented the GET - /health endpoint

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,6 +2,30 @@ import { Router } from 'express';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /health:
+ *  get:
+ *    summary: Health
+ *    description: Sends informations about the process time and the status
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: string
+ *                  example: "ok"
+ *                uptime:
+ *                  type: float
+ *                  example: 52.726634272
+ *                timestamp:
+ *                  type: int
+ *                  example: 1764677909362
+ */
 router.get('/', (_req, res) => {
   res.json({ status: 'ok', uptime: process.uptime(), timestamp: Date.now() });
 });
