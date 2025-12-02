2a04c9d3619e8d43eed8eae8c97c4acaae683594
Loan Nguyen <venel@vmi2942916.contaboserver.net>
2025-12-02 11:22:07 +0100

    docs: Documented the GET - /posts endpoint

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -5,6 +5,39 @@ import { posts } from "../db/schemas.js";
 
 const postRouter = Router();
 
+/**
+ * @openapi
+ * /posts:
+ *  get:
+ *      summary: Get Posts
+ *      description: Retreive all the posts if you are allowed to
+ *      responses:
+ *          200:
+ *              description: Success
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  id:
+ *                                      type: string
+ *                                      example: 2
+ *                                  type:
+ *                                      type: string
+ *                                      enum: ["vertical","video","photo"]
+ *                                      example: "vertical"
+ *                                  platforms:
+ *                                      type: array
+ *                                      items:
+ *                                          type: string
+ *                                      example: ["youtube", "tikok", "instagram"]
+ *                                  mediaurl:
+ *                                      type: string
+ *                                      example: "https://example.com/v/1d23e6z9"
+ *                                           
+ */
 postRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
     try {
         const q = db.select().from(posts) // Arranger le schema afin de pouvoir les ordonner par date
