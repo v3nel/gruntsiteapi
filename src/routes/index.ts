eced06b5b8244d4372328bcc0390370351afff02
Loan Nguyen <venel@vmi2942916.contaboserver.net>
2025-12-02 12:02:02 +0100

    docs: Documented the GET - / endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,27 @@ import healthRouter from './health.js';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /:
+ *  get:
+ *    summary: First
+ *    description: "Gives informations about the api version"
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                name:
+ *                  type: string
+ *                  example: "gruntsiteapi"
+ *                version:
+ *                  type: string
+ *                  example: "0.1.0"
+ */
 router.get('/', (_req, res) => {
   res.json({ name: 'gruntsiteapi', version: '0.1.0' });
 });
